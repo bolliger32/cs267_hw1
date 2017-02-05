04ca8082fcefec2632cfc1ef28a56624a3936a08
bolliger32 <bolliger32@gmail.com>
2017-02-04 16:25:01 -0800

    moved min operations to outer-most loops possible

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -11,9 +11,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* restrict C)
 {
-  /* For each row i of A */
   for (int j = 0; j < N; ++j)
-    /* For each column j of B */ 
     for (int k = 0; k < K; ++k) 
     {
       double bkj = B[k+j*lda];
@@ -32,17 +30,20 @@ void square_dgemm (int lda, double* A, double* B, double* restrict C)
 {
   /* For each block-row of A */ 
   for (int i = 0; i < lda; i += BLOCK_SIZE)
+  {
+    int M = min (BLOCK_SIZE, lda-i);
     /* For each block-column of B */
     for (int j = 0; j < lda; j += BLOCK_SIZE)
+    {
+      int N = min (BLOCK_SIZE, lda-j);
       /* Accumulate block dgemms into block of C */
       for (int k = 0; k < lda; k += BLOCK_SIZE)
       {
-	/* Correct block dimensions if block "goes off edge of" the matrix */
-	int M = min (BLOCK_SIZE, lda-i);
-	int N = min (BLOCK_SIZE, lda-j);
-	int K = min (BLOCK_SIZE, lda-k);
+	    int K = min (BLOCK_SIZE, lda-k);
 
 	/* Perform individual block dgemm */
-	do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+	   do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
       }
+    }
+  }
 }
